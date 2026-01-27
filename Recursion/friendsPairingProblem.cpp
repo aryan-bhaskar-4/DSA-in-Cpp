f06f202c1c0ea8928486d580ebd6a2d055f2847c
Aryan Bhaskar <aryanbhaskar444@gmail.com>
2026-01-27 15:08:21 +0530

    friends Pairing Problem

---
+++ Recursion/friendsPairingProblem.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int friendsPairing(int n){
+    if(n==1 || n==2){
+        return n;
+    }
+
+    return friendsPairing(n-1) + (n-1)*friendsPairing(n-2);
+}
+
+int main(){
+    int n = 4;
+    cout << friendsPairing(n) << endl;
+}
