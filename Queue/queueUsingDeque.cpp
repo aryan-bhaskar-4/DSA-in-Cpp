816a0361ff1d96fd8171fb6baa96cb04a2cacfe3
Aryan Bhaskar <aryanbhaskar444@gmail.com>
2026-02-17 19:38:29 +0530

    Queue Using Deque

---
+++ Queue/queueUsingDeque.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<deque>
+using namespace std;
+
+class Queue {
+    deque<int> deq;
+public:
+    void push(int data){
+        deq.push_back(data);
+    }
+
+    void pop(){
+        deq.pop_front();
+    }
+
+    int front(){
+        return deq.front();
+    }
+
+    bool empty(){
+        return deq.empty();
+    }
+};
+
+int main(){
+    Queue q;
+
+    for(int i=1;i<10;i++){
+        q.push(i);
+    }
+
+    while(!q.empty()){
+        cout << q.front() << ",";
+        q.pop();
+    }
+    return 0;
+}
