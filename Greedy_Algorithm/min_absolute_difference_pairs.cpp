6b7b5019acf4d6387503d871162fc0333e95553d
Aryan Bhaskar <aryanbhaskar444@gmail.com>
2026-02-18 07:44:13 +0530

    Minimum absolute difference pairs

---
+++ Greedy_Algorithm/min_absolute_difference_pairs.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main(){
+    vector<int> A = {4,1,8,7};
+    vector<int> B = {2,3,6,5};
+
+    sort(A.begin(), A.end());
+    sort(B.begin(),B.end());
+
+    int absdiff = 0;
+    for(int i=0;i<A.size();i++){
+        absdiff += abs(A[i] - B[i]);
+    }
+    cout << "Min Absdiff : " << absdiff << endl;
+    return 0;
+}
