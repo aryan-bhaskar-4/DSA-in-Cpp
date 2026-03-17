a899a4506eabf2a3616793666af5f70f6426641c
Aryan Bhaskar <aryanbhaskar444@gmail.com>
2026-03-17 13:03:35 +0530

    Add implementation for level order traversal of a binary tree

---
+++ Binary_Trees/levelorder_traversal.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+using namespace std;
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int data){
+        this->data = data;
+        left = right = NULL;
+    }
+};
+
+static int idx = -1;
+
+Node* buildTree(vector<int> nodes){
+    idx++;
+    if(nodes[idx] == -1){
+        return NULL;
+    }
+    Node* root = new Node(nodes[idx]);
+
+    root->left = buildTree(nodes);
+    root-> right = buildTree(nodes);
+
+    return root;
+}
+
+void levelOrder(Node* root){
+    if(root == NULL){
+        return;
+    }
+
+    queue<Node*> q;
+    q.push(root);
+
+    while(!q.empty()){
+        Node* curr = q.front();
+        q.pop();
+
+        cout << curr->data << " ";
+
+        if(curr->left != NULL){
+            q.push(curr->left);
+        }
+
+        if(curr->right != NULL){
+            q.push(curr->right);
+        }
+    }
+}
+
+int main(){
+    vector<int> nodes = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+
+    Node* root = buildTree(nodes);
+
+    levelOrder(root);
+    cout << endl;
+    return 0;
+}
