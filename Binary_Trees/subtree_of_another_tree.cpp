b5c5a323e2986cee143ce9ec1c4f2dc24109c8f4
Aryan Bhaskar <aryanbhaskar444@gmail.com>
2026-03-18 12:23:29 +0530

    Add implementation for checking if one tree is a subtree of another

---
+++ Binary_Trees/subtree_of_another_tree.cpp
@@ -0,0 +1,78 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Node{
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int data){
+        this->data = data;
+        left = right = NULL;
+    }
+};
+
+static int idx = -1;
+
+Node* buildTree(vector<int> nodes){
+    idx++;
+    if(nodes[idx] == -1){
+        return NULL;
+    }
+
+    Node* curr = new Node(nodes[idx]);
+    curr->left = buildTree(nodes);
+    curr->right = buildTree(nodes);
+
+    return curr;
+}
+
+bool isIdentical(Node* root, Node* root2){
+    if(root == NULL && root2 == NULL){
+        return true;
+    } else if(root == NULL || root2 == NULL){
+        return false;
+    }
+
+    if(root->data != root2->data){
+        return false;
+    }
+
+    return isIdentical(root->left,root2->left) && isIdentical(root->right,root2->right);
+}
+
+bool isSubTree(Node* root, Node* subroot){
+    if(root == NULL && subroot == NULL){
+        return true;
+    } else if(root == NULL || subroot == NULL){
+        return false;
+    }
+
+    if(root->data == subroot->data){
+        if(isIdentical(root,subroot)){
+            return true;
+        }
+    }
+
+    bool isLeftSubtree = isSubTree(root->left, subroot);
+
+    if(!isLeftSubtree){
+        return isSubTree(root->right, subroot);
+    }
+
+    return true;
+}
+
+int main(){
+    vector<int> nodes = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+    Node* root = buildTree(nodes);
+
+    Node* subRoot = new Node(2);
+    subRoot->left = new Node(4);
+    subRoot->right = new Node(5);
+
+    cout << isSubTree(root,subRoot) << endl;
+    return 0;
+}
