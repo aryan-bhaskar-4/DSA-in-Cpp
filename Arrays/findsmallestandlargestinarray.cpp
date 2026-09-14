c015300acc1a6464aa7734192b907ac7c1cc7652
Aryan Bhaskar <aryanbhaskar444@gmail.com>
2026-09-15 01:47:06 +0530

    Refactor variable names for clarity in finding min and max in array

--- Arrays/findsmallestandlargestinarray.cpp
+++ Arrays/findsmallestandlargestinarray.cpp
@@ -5,20 +5,20 @@ using namespace std;
 int main(){
     int arr[] = {4,5,7,3,6,1,7,4,9,2,4,6,7,9};
     int n = sizeof(arr)/sizeof(int);
-    int min = INT_MAX;
-    int max = INT_MIN;
+    int maxNum = INT_MIN;
+    int minNum = INT_MAX;
+
     for(int i=0;i<n;i++){
-        if(arr[i] > max){
-            max = arr[i];
+        if(arr[i] > maxNum){
+            maxNum = arr[i];
         }
 
-        if(arr[i] < min){
-            min = arr[i];
+        if(arr[i] < minNum){
+            minNum = arr[i];
         }
     }
 
-    cout << "Minimum Number : " << min << endl;
-    cout << "Maximum Number : " << max << endl;
-
+    cout << "MAX Num: " << maxNum << endl;
+    cout << "MIN Num: " << minNum << endl;
     return 0;
 }
